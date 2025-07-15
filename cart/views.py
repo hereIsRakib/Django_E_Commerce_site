9212c36ae3e068ecf65e1eacfbb0ce9e6fffe392
hereIsRakib <hossain.rakib8011@gmail.com>
2025-07-16 03:20:18 +0600

    product variation added

--- cart/views.py
+++ cart/views.py
@@ -14,6 +14,9 @@ def _cart_id(request):
     return cart_id
 
 def add_cart(request,product_id):
+
+    color =request.GET('color')
+    size = request.GET('size')
     product = Product.objects.get(id = product_id)
 
     try:
