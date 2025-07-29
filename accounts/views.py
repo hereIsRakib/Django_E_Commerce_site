c48e6a153e332d65137c030e9dc3af02d30afaa6
hereIsRakib <hossain.rakib8011@gmail.com>
2025-07-29 12:26:23 +0600

    forgot password functionality added,login dashboard static

--- accounts/views.py
+++ accounts/views.py
@@ -65,7 +65,7 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             messages.success(request, "You are logged in Successfully")
-            return redirect('login')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid username or password')
             return redirect('login')
@@ -102,3 +102,78 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, "Invalid activation link")
         return redirect('register')
+    
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, "accounts/dashboard.html")
+
+
+def forgotPassword(request):
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact = email)
+
+            #RESET PASSWORD EMAIL
+            current_site = get_current_site(request)
+            mail_subject = "Reset your Password"
+            message = render_to_string('accounts/reset_password_email.html',{
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+                                    
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            messages.success(request, 'password reset link has been sent to your mail address')
+            return redirect('login')
+        
+        else:
+            messages.error(request, 'Account does not exist! ')
+            return redirect('forgotPassword')
+
+    return render(request, 'accounts/forgotPassword.html')
+
+
+def resetPassword_validate(request, uidb64, token):
+
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+
+
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, "Please reset your password")
+        return redirect('resetPassword')
+    
+    else:
+        messages.error(request, 'This link has been expired')
+        return redirect('login')
+
+
+
+def resetPassword(request):
+    if request.method == "POST":
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful ')
+            return redirect('login')
+        else:
+            messages.error(request, "Password does not match! ")
+            return redirect('resetPassword')
+        
+    else:
+        return render(request, 'accounts/resetPassword.html')
